07a32344c3f6cc3aeb1c3306e4a24ea41b206d27
jose jacobo <jose.jacobo@globant.com>
2024-01-21 16:58:39 -0600

    CreateTable - UseCase

--- src/app.logic.ts
+++ src/app.logic.ts
@@ -1,25 +1,26 @@
 import fs from 'fs';
+import { yarg } from "./config/plugins/args.plugin";
 
-
+const { b:base, l:limit, s:showTable } = yarg;
 let outputMessage: string = '';
-const base: number = 9;
-const headerMessage: string= `
+const headerMessage: string = `
 ======================================
           Tabla del ${base}
 ======================================\n
 `;
 
-for (let i = 0; i <= 10; i++) {
-    outputMessage += `${base} * ${i} = ${base*i}\n`
+for (let i = 0; i <= limit; i++) {
+    outputMessage += `${base} * ${i} = ${base * i}\n`;
 }
 
 outputMessage = headerMessage + outputMessage;
-console.log(outputMessage);
+
+if(showTable) {
+    console.log(outputMessage);
+}
 
 const outputPath = `outputs`
 
 fs.mkdirSync(outputPath, { recursive: true })
-
 fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
-
 console.log('File created!');
---
+++ src/domain/use-cases/create-table.use-case.ts
@@ -0,0 +1,26 @@
+export interface CreateTableUseCase {
+    execute: (options: CreateTableOptions) => string;
+}
+
+export interface CreateTableOptions {
+    base: number;
+    limit?: number;
+}
+
+
+export class CreateTable implements CreateTableUseCase {
+    constructor(
+        /**
+         * DI - Dependency Injection
+         */
+    ) { }
+
+    execute({ base, limit = 10 }: CreateTableOptions) {
+        let outputMessage = '';
+        for (let i = 0; i <= limit; i++) {
+            outputMessage += `${base} * ${i} = ${base * i}\n`;
+        }
+
+        return outputMessage;
+    }
+}
